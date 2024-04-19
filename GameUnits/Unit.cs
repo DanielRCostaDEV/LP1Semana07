9fb86eaab8e8c6480f7fc35a3f9a52ea728f3dbf
Daniel Costa <danielrcosta.dev@gmail.com>
2024-04-19 13:08:44 +0100

    XPUnit

--- GameUnits/Unit.cs
+++ GameUnits/Unit.cs
@@ -13,6 +13,21 @@ namespace GameUnits
             Health = health;
         }
 
+            public abstract class XPUnit : Unit
+    {
+        public int XP { get; protected set; }
+
+        public XPUnit(int movement, int health) : base(movement, health)
+        {
+            XP = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} XP={XP}";
+        }
+    }
+
         public abstract float Cost { get; }
 
         public void Move()
